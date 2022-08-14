a6e3c3f334640766901958205dbed21943405c6f
Anton Romanov <rad250495@yandex.ru>
2022-08-14 20:53:28 +0700

    feat: add HiddenMenu adaptiv markup

--- src/components/Header/MoviesNav/MoviesNav.js
+++ src/components/Header/MoviesNav/MoviesNav.js
@@ -1,41 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
+import  HiddenMenu  from "./HiddenMenu/HiddenMenu";
 import "./MoviesNav.css";
 
 export default function MoviesNav() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  console.log(menuIsOpen)
+
+  const toggleDisplayMenu = () => {
+    setMenuIsOpen(!menuIsOpen);
+  };
+
   return (
-    <ul className="movies-nav">
-      <li>
-        <NavLink
-          to="/movies"
-          className={({ isActive }) =>
-            isActive
-              ? "movies-nav__link movies-nav__link_active"
-              : "movies-nav__link"
-          }
-        >
-          Фильмы
-        </NavLink>
-      </li>
+    <>
+      <ul className="movies-nav">
+        <li>
+          <NavLink
+            to="/movies"
+            className={({ isActive }) =>
+              isActive
+                ? "movies-nav__link movies-nav__link_active"
+                : "movies-nav__link"
+            }
+          >
+            Фильмы
+          </NavLink>
+        </li>
 
-      <li>
-        <NavLink
-          to="/saved-movies"
-          className={({ isActive }) =>
-            isActive
-              ? "movies-nav__link movies-nav__link_active"
-              : "movies-nav__link"
-          }
-        >
-          Сохраненные фильмы
-        </NavLink>
-      </li>
+        <li>
+          <NavLink
+            to="/saved-movies"
+            className={({ isActive }) =>
+              isActive
+                ? "movies-nav__link movies-nav__link_active"
+                : "movies-nav__link"
+            }
+          >
+            Сохраненные фильмы
+          </NavLink>
+        </li>
 
-      <li>
-        <Link to="/profile" className="movies-nav__link movies-nav__link_btn">
-          Аккаунт
-        </Link>
-      </li>
-    </ul>
+        <li>
+          <Link to="/profile" className="movies-nav__link movies-nav__link_btn">
+            Аккаунт
+          </Link>
+        </li>
+      </ul>
+      <div className="movies-nav-hidden" onClick={toggleDisplayMenu}>
+        {menuIsOpen && <HiddenMenu />}
+      </div>
+    </>
   );
 }
